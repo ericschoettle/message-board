b3bea998facfbdf8572e0fa30f10ea33a042c734
ericschoettle <eric.schoettle@gmail.com>
2017-06-14 20:44:00 -0700

    Add delete function to answers

--- app/components/answer-view.js
+++ app/components/answer-view.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    destroyAnswer() {
+    destroyAnswer(answer) {
       this.sendAction('destroyAnswer', answer)
     }
   }
